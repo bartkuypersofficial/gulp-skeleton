9c2c9bf5b685399eb1281e1b8b789036a37f9f96
bartkuypersofficial <info@bartkuypers.nl>
2018-08-11 19:31:48 +0200

    added sass-lint function

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
-    autoprefixer = require('gulp-autoprefixer');
+    autoprefixer = require('gulp-autoprefixer'),
+    sasslint = require('gulp-sass-lint');
 
 var config = {
     autoprefixer: { browsers: ['> 2%'], grid: true }
@@ -8,6 +9,9 @@ var config = {
 
 gulp.task('sass', function() {
   return gulp.src('scss/styles.scss')
+    .pipe(sasslint({configFile: '.sass-lint.yml'}))
+    .pipe(sasslint.format())
+    .pipe(sasslint.failOnError())
     .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
     .pipe(autoprefixer(config.autoprefixer))
     .pipe(gulp.dest('css'));
