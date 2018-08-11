457f08b6c1618a1582abc63e0021da171fa3b493
bartkuypersofficial <info@bartkuypers.nl>
2018-08-11 18:58:54 +0200

    setting up the default

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,20 @@
 var gulp = require('gulp'),
-    sass = require('gulp-sass');
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer');
+
+var config = {
+    autoprefixer: { browsers: ['> 2%'], grid: true }
+}
 
 gulp.task('sass', function() {
   return gulp.src('scss/styles.scss')
     .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+    .pipe(autoprefixer(config.autoprefixer))
     .pipe(gulp.dest('css'));
 });
 
 gulp.task('watch', ['sass'], function() {
-  gulp.watch(['scss/**/*'], ['sass']);
+  gulp.watch(['scss/**/*.scss'], ['sass']);
 });
 
 gulp.task('default', ['watch']);
