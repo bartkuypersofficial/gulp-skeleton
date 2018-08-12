8bcb300b4ca56a3be60a9a644d0c73db6364cb65
bartkuypersofficial <info@bartkuypers.nl>
2018-08-12 15:12:22 +0200

    Added the ideal sass-lint configuration

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,42 @@
+'use strict';
+
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
     autoprefixer = require('gulp-autoprefixer'),
-    sasslint = require('gulp-sass-lint');
-
-var config = {
-    autoprefixer: { browsers: ['> 2%'], grid: true }
-}
+    sassLint = require('gulp-sass-lint');
 
 gulp.task('sass', function() {
   return gulp.src('scss/styles.scss')
-    .pipe(sasslint({configFile: '.sass-lint.yml'}))
-    .pipe(sasslint.format())
-    .pipe(sasslint.failOnError())
     .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
-    .pipe(autoprefixer(config.autoprefixer))
+    .pipe(autoprefixer({
+      browsers: [
+        'last 1 major version',
+        '>= 1%',
+        'Chrome >= 45',
+        'Firefox >= 38',
+        'Edge >= 12',
+        'Explorer >= 10',
+        'iOS >= 9',
+        'Safari >= 9',
+        'Android >= 4.4',
+        'Opera >= 30'
+      ],
+      grid: true
+    }))
     .pipe(gulp.dest('css'));
 });
 
-gulp.task('watch', ['sass'], function() {
-  gulp.watch(['scss/**/*.scss'], ['sass']);
+gulp.task('sass-lint', function () {
+    return gulp.src([
+        'scss/**/*.scss'
+    ])
+    .pipe(sassLint())
+    .pipe(sassLint.format())
+    .pipe(sassLint.failOnError())
+});
+
+gulp.task('watch', ['sass', 'sass-lint'], function() {
+  gulp.watch(['scss/**/*.scss'], ['sass', 'sass-lint']);
 });
 
 gulp.task('default', ['watch']);
