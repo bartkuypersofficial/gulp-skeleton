fac982879209952837ab42491b05cc018c0323be
bartkuypersofficial <info@bartkuypers.nl>
2018-10-06 17:20:53 +0200

    Install gulp minify options for scripts and styles

--- assets/gulpfile.js
+++ assets/gulpfile.js
@@ -4,6 +4,9 @@ var gulp = require('gulp'),
     sass = require('gulp-sass'),
     concat = require('gulp-concat'),
     autoprefixer = require('gulp-autoprefixer'),
+    minify = require('gulp-minify'),
+    rename = require('gulp-rename'),
+    cssmin = require('gulp-cssmin'),
     sassLint = require('gulp-sass-lint');
 
 gulp.task('sass', function() {
@@ -28,9 +31,29 @@ gulp.task('scripts', function() {
     .pipe(gulp.dest('../js'));
 });
 
-gulp.task('watch', ['sass', 'sass-lint', 'scripts'], function() {
-  gulp.watch(['scss/**/*.scss'], ['sass', 'sass-lint']);
-  gulp.watch(['scripts/**'], ['scripts']);
+gulp.task('minify-scripts', function() {
+  return gulp.src('scripts/**')
+    .pipe(concat('scripts.js'))
+    .pipe(minify({
+      ext:{
+        min:'.min.js'
+      }
+    }))
+    .pipe(gulp.dest('../js'));
+});
+
+gulp.task('minify-styles', function() {
+  return gulp.src('scss/styles.scss')
+    .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+    .pipe(autoprefixer({browsers: ['>= 2%'], grid: true}))
+    .pipe(cssmin())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('../css'));
+});
+
+gulp.task('watch', ['sass', 'sass-lint', 'minify-styles', 'scripts', 'minify-scripts' ], function() {
+  gulp.watch(['scss/**/*.scss'], ['sass', 'sass-lint', 'minify-styles']);
+  gulp.watch(['scripts/**'], ['scripts', 'minify-scripts']);
 });
 
 gulp.task('default', ['watch']);
